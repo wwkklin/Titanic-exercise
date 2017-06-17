f4f3154acf0987ed7670dfa9195a742b22563ef0
wwkklin <wwkklin@gmail.com>
2017-06-17 14:12:01 -0700

    commit

---
+++ TitanicExercise.R
@@ -0,0 +1,12 @@
+# titanic is avaliable in your workspace
+# 1 - Check the structure of titanic
+str(titanic)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
+  geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic, aes(x = Pclass, fill = Sex)) + 
+  geom_bar(position = "dodge") + 
+  facet_grid(. ~ Survived)
